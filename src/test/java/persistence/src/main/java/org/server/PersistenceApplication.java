3ba5db342b811fac09d05cd7d15384c4b3a697ea
Santiago Velandia Gallo <santiago.velandiag@uqvirtual.edu.co>
2023-10-25 13:14:38 -0500

    basic configuration app

---
+++ src/test/java/persistence/src/main/java/org/server/PersistenceApplication.java
@@ -0,0 +1,12 @@
+package org.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class PersistenceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PersistenceApplication.class, args);
+    }
+}
